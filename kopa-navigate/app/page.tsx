c16259632458feac37fd4fa5a17ab598c1350048
tedph-cyber <theooye7@gmail.com>
2025-02-27 22:16:34 +0100

    about?

--- kopa-navigate/app/page.tsx
+++ kopa-navigate/app/page.tsx
@@ -1,21 +1,20 @@
-
 import Hero from "@/components/Hero/hero";
 import HeaderPage from "../components/Header/headerPage";
 import Statedisplay from "./components/State-display/displayState";
-
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <>
-    <HeaderPage/>
-    
-
-    
-      <Hero/>
-     <div className="bg-white" >
-      <Statedisplay />
-     
+    <div>
+      <HeaderPage />
+      <Head>
+        <link rel="shortcut icon" href="/img/nysc.png" type="image/png" />
+        <link rel="shortcut icon" href="favicon.png" type="image/png" />
+      </Head>
+      <Hero />
+      <div className="bg-white">
+        <Statedisplay />
+      </div>
     </div>
-    </>
   );
 }
